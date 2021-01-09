30cda579be8248efee07991c1e63afe65500f91f
angelvillarreal24 <angel.villarreal.24@bishops.com>
2021-01-09 11:24:43 -0800

    Finished next 1.9 steps in League Invaders
    FInished 3.9 steps in total
    .9 because text size and location.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,8 +2,46 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
+    Font titleFont;
     @Override
     public void paintComponent(Graphics g){
-        g.fillRect(10, 10, 100, 100);
+        titleFont = new Font("Arial", Font.PLAIN, 48);
+        if(currentState == MENU){
+            drawMenuState(g);
+        }else if(currentState == GAME){
+            drawGameState(g);
+        }else if(currentState == END){
+            drawEndState(g);
+        }
+    }
+    void updateMenuState() {
+
+    }
+    void updateGameState() {
+
+    }
+    void updateEndState()  {
+
+    }
+    void drawMenuState(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(0, 0, LeagueInvaders.gFrameWIDTH, LeagueInvaders.gFrameHEIGHT);
+        g.setFont(titleFont);
+        g.setColor(Color.YELLOW);
+        g.drawString("LEAGUE INVADERS", 1, 100);
+        g.drawString("Press ENTER to Start", 1, 500);
+        g.drawString("Press SPACE for Instructions", 1, 700);
+    }
+    void drawGameState(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, LeagueInvaders.gFrameWIDTH, LeagueInvaders.gFrameHEIGHT);
+    }
+    void drawEndState(Graphics g)  {
+        g.setColor(Color.RED);
+        g.fillRect(0, 0, LeagueInvaders.gFrameWIDTH, LeagueInvaders.gFrameHEIGHT);
     }
 }
